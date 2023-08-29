5543ce55981f3a316586ca988a24a580db10f7f4
theanirbanpanda <anirbanpandaxyz@gmail.com>
2023-08-29 21:47:18 +0530

    code-added

---
+++ To-Do List/script.js
@@ -0,0 +1,51 @@
+
+let addbtn=document.getElementById("addbtn");
+addbtn.addEventListener("click",()=>{
+
+item=document.getElementById("textfield").value;
+if(!item)
+{
+    alert("please enter something");
+  
+   
+}
+else{
+  
+var text = document.createTextNode(item);
+var newItem = document.createElement('li');
+
+// Create checkbox
+var checkbox = document.createElement('input');
+checkbox.type = 'checkbox';
+newItem.appendChild(checkbox);
+checkbox.style.marginRight='20px';
+
+// Append text to list item
+newItem.appendChild(document.createTextNode(item));
+
+// Create cross button
+var crossButton = document.createElement('button');
+crossButton.innerHTML = 'X';
+crossButton.style.marginLeft='250px';
+crossButton.style.border='0';
+crossButton.style.background='transparent';
+  crossButton.addEventListener('click', function() {
+    newItem.remove();
+  });
+newItem.appendChild(crossButton);
+
+    // Add item to list
+    document.getElementById('todolist').appendChild(newItem);
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+          newItem.style.textDecoration = 'line-through';
+        } else {
+          newItem.style.textDecoration = 'none';
+        }
+
+      });
+      document.getElementById('textfield').value = '';
+    }
+})
+  
+  
